2f2a915113a52603e68282916187458d3304e459
Gitesh Sethi <113576113+Gittu10@users.noreply.github.com>
2026-06-22 13:53:44 +0530

    Add files via upload

---
+++ Platform.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.models
+
+data class Platform(
+    val id: Double,
+    val name: String,
+    val slug: String,
+    val symbol: String,
+    val token_address: String
+)
